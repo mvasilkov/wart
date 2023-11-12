3c3922b8cfe76a5a4b0b87c20ca9735256355c55
Mark Vasilkov <mvasilkov@gmail.com>
2023-11-12 13:23:50 +0200

    Init

---
+++ wart_rs/src/lib.rs
@@ -0,0 +1,22 @@
+use js_sys;
+use wasm_bindgen::prelude::*;
+use wast::lexer::Lexer;
+
+#[wasm_bindgen]
+pub fn lexer(wat: &str, callback: &js_sys::Function) {
+    let lexer = Lexer::new(&wat);
+    for token_result in lexer.iter(0) {
+        match token_result {
+            Ok(token) => {
+                let kind = js_sys::JsString::from(format!("{:?}", token.kind));
+                let offset = js_sys::Number::from(token.offset as u32);
+                let len = js_sys::Number::from(token.len);
+                let _ = callback.call3(&JsValue::NULL, &kind, &offset, &len);
+            }
+            Err(err) => {
+                let err = js_sys::Error::new(&err.to_string());
+                let _ = callback.call1(&JsValue::NULL, &err);
+            }
+        }
+    }
+}
