3137a229522f1379643de322cb54efd957c2f907
Mark Vasilkov <mvasilkov@gmail.com>
2023-11-15 15:10:56 +0200

    Passthrough

--- wart/app.js
+++ wart/app.js
@@ -1,22 +1,53 @@
 'use strict'
 
 const fs = require('node:fs/promises')
+const { program } = require('commander')
+const { version } = require('./package.json')
 
-async function run() {
+const createLexer = async () => {
     const { default: init, lexer } = await import('wart_rs')
 
-    await init(fs.readFile(require.resolve('wart_rs/wart_rs_bg.wasm')))
+    const wasm = require.resolve('wart_rs/wart_rs_bg.wasm')
+    await init(fs.readFile(wasm))
 
-    lexer('(module (func))', (kind, offset, len, text) => {
-        if (kind instanceof Error) {
-            console.error(kind)
-            return
-        }
-        // offset and len are in bytes
-        console.log('%o %o %o %o', kind, offset, len, text)
-    })
+    return lexer
 }
 
-if (require.main === module) {
-    run()
+let lexer = null
+
+const fmt = async wasm => {
+    if (!lexer) {
+        [lexer, wasm] = await Promise.all([createLexer(), wasm])
+    }
+    else wasm = await wasm
+
+    const result = []
+
+    lexer(wasm, (kind, text) => {
+        if (kind instanceof Error) throw kind
+
+        result.push(text)
+    }, false)
+
+    return result.join('')
 }
+
+const cli = async files => {
+    for (const file of files) {
+        const wasm = fs.readFile(file, 'utf8')
+        const result = await fmt(wasm)
+
+        console.log(result)
+    }
+}
+
+const run = async () => {
+    program
+        .version(version)
+        .argument('<files...>')
+        .action(cli)
+        .parse()
+}
+
+if (require.main === module) run()
+else module.exports = fmt
